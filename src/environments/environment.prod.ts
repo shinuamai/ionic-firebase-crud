d3562875e7792eec8c649958b29e7a65701dc9af
shinuamai <laurad00sandovalg@gmail.com>
2023-09-01 13:50:32 -0500

    firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCHvoixX1FAmeXgW04FVgtSLzNfwwvSNug",
+    authDomain: "promovil-48467.firebaseapp.com",
+    databaseURL: "https://promovil-48467-default-rtdb.firebaseio.com",
+    projectId: "promovil-48467",
+    storageBucket: "promovil-48467.appspot.com",
+    messagingSenderId: "801106417494",
+    appId: "1:801106417494:web:5603ecfff63cd4746e28f7"
+  }
 };
